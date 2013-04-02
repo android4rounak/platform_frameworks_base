d25d2a1c8d25cf3fd9e5ba6a7033af9c0b12c7b1
Royce Jiang <jiangxd@embedinfo.com>
2013-04-02 15:20:35 +0800

    Revert "libagl: add support for copybit"
    This reverts commit dd09a329f8d5e84a27d3c350efe556762fb1560d.

--- opengl/libagl/Android.mk
+++ opengl/libagl/Android.mk
@@ -6,21 +6,16 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-
-# Set to 1 to use gralloc and copybits
-LIBAGL_USE_GRALLOC_COPYBITS := 1
-
 LOCAL_SRC_FILES:= \
-	egl.cpp                         \
-	state.cpp		        \
-	texture.cpp		        \
-	Tokenizer.cpp                   \
-	TokenManager.cpp                \
-	TextureObjectManager.cpp        \
-	BufferObjectManager.cpp         \
+	egl.cpp                     \
+	state.cpp		            \
+	texture.cpp		            \
+    Tokenizer.cpp               \
+    TokenManager.cpp            \
+    TextureObjectManager.cpp    \
+    BufferObjectManager.cpp     \
 	array.cpp.arm		        \
-	fp.cpp.arm		        \
+	fp.cpp.arm		            \
 	light.cpp.arm		        \
 	matrix.cpp.arm		        \
 	mipmap.cpp.arm		        \
@@ -30,8 +25,6 @@ LOCAL_SRC_FILES:= \
 LOCAL_CFLAGS += -DLOG_TAG=\"libagl\"
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
 LOCAL_CFLAGS += -fvisibility=hidden
-
-LOCAL_PRELINK_MODULE := false
 
 LOCAL_SHARED_LIBRARIES := libcutils libhardware libutils libpixelflinger libETC1
 LOCAL_LDLIBS := -lpthread -ldl
@@ -51,18 +44,9 @@ endif
 ifeq ($(TARGET_ARCH)-$(ARCH_ARM_HAVE_TLS_REGISTER),arm-true)
     LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
 endif
-
 LOCAL_C_INCLUDES += bionic/libc/private
 
-ifeq ($(LIBAGL_USE_GRALLOC_COPYBITS),1)
-    LOCAL_CFLAGS += -DLIBAGL_USE_GRALLOC_COPYBITS
-    LOCAL_SRC_FILES += copybit.cpp
-    LOCAL_SHARED_LIBRARIES += libui
-endif
-
-
-
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/egl
-LOCAL_MODULE:= libGLES_ATMEL_SAM
+LOCAL_MODULE:= libGLES_android
 
 include $(BUILD_SHARED_LIBRARY)
--- opengl/libagl/context.h
+++ opengl/libagl/context.h
@@ -15,37 +15,6 @@
 ** limitations under the License.
 */
 
-#ifndef ANDROID_CONTEXT_INTERFACE_H
-#define ANDROID_CONTEXT_INTERFACE_H
-
-#include "gl_context.h"
-#include <ui/Region.h>
-
-namespace android {
-
-class Region;
-// ---------------------------------------------------------------------------
-
-struct region_iterator : public copybit_region_t {
-    region_iterator(const Region& region)
-        : b(region.begin()), e(region.end()) {
-        this->next = iterate;
-    }
-private:
-    static int iterate(copybit_region_t const * self, copybit_rect_t* rect) {
-        region_iterator const* me = static_cast<region_iterator const*>(self);
-        if (me->b != me->e) {
-            *reinterpret_cast<Rect*>(rect) = *me->b++;
-            return 1;
-        }
-        return 0;
-    }
-    mutable Region::const_iterator b;
-    Region::const_iterator const e;
-};
-
-}
+#include <private/opengles/gl_context.h>
 
 using namespace android::gl;
-
-#endif //ANDROID_CONTEXT_INTERFACE_H
